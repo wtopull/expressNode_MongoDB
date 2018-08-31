2f155b39710099221c26c5de0cd74d4222d7767f
BS-20180731BRSG\Administrator <wtopull@gmail.com>
2018-08-31 15:24:23 +0800

    nathan

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/runoob";
+
+
+MongoClient.connect(url, function (err, db) {
+  if (err) throw err;
+  // var dbo = db.db("runoob");
+  // var myobj = {
+  //   name: "菜鸟教程",
+  //   url: "www.runoob",
+  // };
+  // dbo.collection("site").insertOne(myobj, function (err, res) {
+  //   if (err) throw err;
+  //   console.log("文档插入成功");
+  //   db.close();
+  // });
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("runoob");
+    dbo.collection("site").findOne({}, function (err, result) {
+      if (err) throw err;
+      console.log(result);
+       res.render('index', {
+         title: result.doctor
+       });
+      db.close();
+    });
+  });
+ 
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  
+  var url = "mongodb://localhost:27017/";
+
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("runoob");
+    dbo.collection("site").findOne({}, function (err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+    });
+  });
 });
 
 module.exports = router;
